f79a243e5030547efb59ed563cdfa15ef20d111b
Daniel Garcia - Jodadev <jodadev.col@gmail.com>
2025-11-23 21:14:54 +1100

    Update job listings and button styles in searching page

--- mod-01-javascript/main.js
+++ mod-01-javascript/main.js
@@ -1,8 +1,24 @@
-const jobs = document.querySelectorAll('.searching-results .result .result-action>a')
+// Code for event by every button with foreach
+// const jobs = document.querySelectorAll('.searching-results .result .result-action>button')
 
-jobs.forEach((job) => {
-  job.addEventListener('click', function () {
+// jobs.forEach((jobButton) => {
+//   jobButton.addEventListener('click', function (event) {
+//     jobButton.classList.add('is-applied')
+//     jobButton.innerHTML = 'Aplicado'
+//     jobButton.disabled = true
+//   })
+// })
 
-    console.log('Job clicked')
-  })
+// Code for event delegation
+const jobsContainer = document.querySelector('.searching-results-container>.searching-results')
+console.log(jobsContainer)
+
+jobsContainer?.addEventListener('click', function (event) {
+  const element = event.target
+
+  if (element.classList.contains('result-apply-job')) {
+    element.classList.add('is-applied')
+    element.innerHTML = 'Aplicado'
+    element.disabled = true
+  }
 })
